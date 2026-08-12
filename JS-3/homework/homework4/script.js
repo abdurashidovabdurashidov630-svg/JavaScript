b785101b6834ddf17e42e409722cb3430059eff8
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-08-12 07:56:35 +0500

    Video darslik

---
+++ JS-3/homework/homework4/script.js
@@ -0,0 +1,98 @@
+// let a = [
+//   { id: 1, type: "kirim", amount: 500000 },
+//   { id: 2, type: "chiqim", amount: 120000 },
+//   { id: 3, type: "kirim", amount: 900000 },
+// ];
+// a.forEach((value) => {
+//   console.log(
+//     `#${value.id} | ${value.type.toUpperCase()} | ${value.amount}so'm`,
+//   );
+// });
+
+// let students = [
+//   { name: "Ali", ball: 78 },
+//   { name: "Vali", ball: 42 },
+//   { name: "Sami", ball: 91 },
+// ];
+// let a = 0;
+
+// students.forEach((value) => {
+//   let status = value.ball > 60 ? "O'tdi" : "Yiqildi";
+//   console.log(`${value.name} - ${status}`);
+// });
+
+// let products = [
+//   { name: "Shakar", count: 12 },
+//   { name: "Yog'", count: 3 },
+//   { name: "Un", count: 25 },
+// ];
+
+// products.forEach((value) => {
+//   if (value.count < 5)
+//     console.log(`${value.name} - ${value.count} ta (Kam qoldi)`);
+//   else console.log(`${value.name} - ${value.count} ta`);
+// });
+
+// let users = [
+//   { name: "Ali", age: 20 },
+//   { name: "Vali", age: 25 },
+// ];
+
+// let sum88 = users.map((value) => {
+//   return `${value.name} (${value.age} yosh)`;
+// });
+
+// console.log(sum88);
+
+// let prices = [100000, 250000, 80000];
+
+// let sum09 = prices.map((value) => {
+//   return value * 12;
+// });
+
+// console.log(sum09);
+
+// let names = ["Ali Valiyev", "Sami Karimov", "Olim Hasanov"];
+
+// let sum00 = names.map((value) => {
+//   return value.toLowerCase().replaceAll(" ", "_");
+// });
+
+// console.log(sum00);
+
+// let activeUsers = [
+//   { name: "Ali", active: true },
+//   { name: "Vali", active: false },
+//   { name: "Sami", active: true },
+// ];
+
+// let sum01 = activeUsers.filter((value) => {
+//   return value.active === true;
+// });
+
+// console.log(sum01);
+
+// let students0 = [
+//   { name: "Ali", ball: 56 },
+//   { name: "Vali", ball: 88 },
+//   { name: "Sami", ball: 71 },
+//   { name: "Olim", ball: 44 },
+// ];
+
+// let sum = students0.filter((value) => {
+//   return value.ball >= 60;
+// });
+
+// console.log(sum);
+
+// let products0 = [
+//   { name: "Telefon", price: 3500000 },
+//   { name: "Quloqchin", price: 250000 },
+//   { name: "Noutbuk", price: 7800000 },
+// ];
+
+// let sum0 = products0.filter((value) => {
+//   return value.price > 100000;
+// });
+
+// console.log(sum0);
